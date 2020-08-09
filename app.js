9143faefd5b17a26d73d9cb3da770e06f0cb808f
sanket <sanket.naik@luxactuaries.com>
2020-08-09 10:23:52 +0530

    changes

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 var cors = require('cors');
 
-app.use(cors());
+app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+});
 dotenv.config()
 
 const QuoteApi = require('./QuoteAPI')
